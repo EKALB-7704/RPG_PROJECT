6fb3abad5c8a4549a25102af7cbddb2a073ec033
charlielambe31-eng <charlielambe31@gmail.com>
2025-12-02 16:38:32 +0000

    moving player information

--- player.h
+++ player.h
@@ -23,4 +23,43 @@ public:
     void gainExp(int amount);
 };
 
+class Character
+{
+    // All Void Functions Are Capitalized
+    private:
+        void Age();
+        void Gender();
+        void Race();
+        void Name();
+        void Class_();
+
+    int level= 0;
+    int base_health= 100;
+    int base_strength = 10;
+    int base_defense = 10;
+    int gold = 0;
+
+    
+
+    public:
+    Character() = default;  // Default constructor
+    void Create();
+    void Character_Readback();
+    void starting_stats();
+    void stats_readback();
+    void create_prompt();
+
+    
+    int age;
+    int Class;
+    string name;
+    string gender;
+    string Create_choice;
+    int race;
+
+  
+    char confirmation;
+     int m,M,f,F;
+};
+
 #endif
